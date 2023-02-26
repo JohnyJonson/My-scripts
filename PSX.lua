f514e4af3a87876a571b3bb2f8ebcb19d0c2b574
JohnyJonson <118004367+JohnyJonson@users.noreply.github.com>
2023-02-26 11:17:52 +0100

    Update and rename FakeSign to PSX.lua

--- PSX.lua
+++ PSX.lua
@@ -13,8 +13,19 @@ IntroIcon = <string> - URL to the image you want to use in the intro animation.
 Icon = <string> - URL to the image you want displayed on the window.
 CloseCallback = <function> - Function to execute when the window is closed.
 ]]
+local Tab2 = Window:MakeTab({
+	Name = "HoverBoards",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--[[
+Name = <string> - The name of the tab.
+Icon = <string> - The icon of the tab.
+PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
+]]
 local Tab = Window:MakeTab({
-	Name = "Partner Spoof",
+	Name = "Main",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
@@ -66,4 +77,16 @@ Time = <number> - The duration of the notfication.
 Name = <string> - The name of the toggle.
 Default = <bool> - The default value of the toggle.
 Callback = <function> - The function of the toggle.
+]]
+
+Tab2:AddButton({
+	Name = "Get All HoverBoards",
+	Callback = function()
+      		local tbl = require(game.ReplicatedStorage.Framework.Modules.Client["4 | Save"]).Get() tbl.Hoverboards = {} for i,v in pairs(game.ReplicatedStorage.Game.Hoverboards:GetChildren())do table.insert(tbl.Hoverboards, v.Name) end getsenv(game.Players.LocalPlayer.PlayerScripts.Scripts.GUIs.Hoverboards).Update() getsenv(game.Players.LocalPlayer.PlayerScripts.Scripts.GUIs.Hoverboards).Equip = function(board) tbl.EquippedHoverboard = board getsenv(game.Players.LocalPlayer.PlayerScripts.Scripts.GUIs.Hoverboards).Update() end
+  	end    
+})
+
+--[[
+Name = <string> - The name of the button.
+Callback = <function> - The function of the button.
 ]]
