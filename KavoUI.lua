110fdd16620c3b9160ac575a127d3560ebf57c42
JohnyJonson <118004367+JohnyJonson@users.noreply.github.com>
2023-02-27 18:22:04 +0100

    Update KavoUI.lua

--- KavoUI.lua
+++ KavoUI.lua
@@ -1,16 +1,23 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local colors = {
-   SchemeColor = Color3.fromRGB(122, 203, 17)
-Background = Color3.fromRGB(134, 158, 95)
-Header = Color3.fromRGB(90, 136, 17)
-TextColor = Color3.fromRGB(255,255,255)
-ElementColor = Color3.fromRGB(0, 255, 127)
+   SchemeColor = Color3.fromRGB(122, 203, 17),
+Background = Color3.fromRGB(0,0,24),
+Header = Color3.fromRGB(0,0,29),
+TextColor = Color3.fromRGB(255,255,255),
+ElementColor = Color3.fromRGB(0,0,100)
 
 }
 
-local Window = Library.CreateLib("🔥ROFAST🔥", colors)
+local Window = Library.CreateLib("🔥ROFAST🔥 📢📢🔥😎✅⚪🔥🔥UNIVERSAL👀✅👀🔥🔥🔥🔥👑💀", colors)
 local Tab = Window:NewTab("✅Humanoid✅")
 local Section = Tab:NewSection("MainFunctions")
 Section:NewButton("🔥SPEED🔥", "Increases WalkSpeed, Reset To  get normal speed", function()
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
+end)
+Sectionv:NewToggle("✅EXPLOITV2", "v3", function(state)
+    if state then
+          game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
+    else
+           game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+    end
 end)
